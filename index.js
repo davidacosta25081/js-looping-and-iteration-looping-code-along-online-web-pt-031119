a9a5ceb82d59c9ab24437b194699788eb1bf0d6e
David Acosta <davidacosta25081@yahoo.com>
2019-10-07 21:36:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solutions in this file
+const names = ["Ada", "Brendan", "Ali"];
+const event = ["birthday"];
+
+function writeCards(names,event){
+for (let i = 0 ; i < names.length; i++){
+  console.log('Thank you, ${names[i]}, for the wonderful ${event[0]} gift!');
+}
+return names;
+
+
+writeCards(names,event)
