66411f53a6039cba56f4fb7466afe0d1a21d7f32
David Acosta <davidacosta25081@yahoo.com>
2019-10-10 06:29:24 +0000

    Done.

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const event = ["birthday"];
 
 function writeCards(names,event){
 for (let i = 0 ; i < names.length; i++){
-  console.log('Thank you, ${names[i]}, for the wonderful ${event[0]} gift!');
+  console.log(`Thank you, ${names[i]}, for the wonderful ${event[0]} gift`);
 }
 return names;
+}
 
-
-writeCards(names,event)
+writeCards(names,event);
